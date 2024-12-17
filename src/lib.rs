64d89d68972b9fc4652829397dc2aca341dcb835
海贵青 <haigq@neunit.com>
2024-12-17 18:54:06 +0800

    feat: add Voice Activity Detection functionality

--- src/lib.rs
+++ src/lib.rs
@@ -1,14 +1,25 @@
-pub fn add(left: u64, right: u64) -> u64 {
-    left + right
-}
+//! The `tinker-vad` library provides Voice Activity Detection (VAD) functionality.
 
-#[cfg(test)]
-mod tests {
-    use super::*;
+use candle_core::{
+  utils::{cuda_is_available, metal_is_available},
+  Device,
+};
 
-    #[test]
-    fn it_works() {
-        let result = add(2, 2);
-        assert_eq!(result, 4);
-    }
+pub mod vad;
+pub use vad::*;
+
+/// Selects and returns an available `Device`.
+/// If `force_cpu` is true, it will always return a CPU device.
+/// Otherwise, it will attempt to return a CUDA or Metal device if available,
+/// falling back to a CPU device if neither is available.
+pub fn select_device(force_cpu: bool) -> Result<Device> {
+  if force_cpu {
+    Ok(Device::Cpu)
+  } else if cuda_is_available() {
+    Ok(Device::new_cuda(0)?)
+  } else if metal_is_available() {
+    Ok(Device::new_metal(0)?)
+  } else {
+    Ok(Device::Cpu)
+  }
 }
